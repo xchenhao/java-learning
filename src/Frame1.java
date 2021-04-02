afcb31f40ae51fa3d0d7af2c60a7e38b642376d7
xchenhao <chenhao_apple@163.com>
2021-04-03 00:33:59 +0800

    接口回调

--- src/Frame1.java
+++ src/Frame1.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Frame1 {
+public class Frame1 extends Frame implements Frame2.MoneyListener {
+
+    public static void main(String[] args) {
+        new Frame1();
+    }
+
+    private Label label = new Label("金额");
+    private Button btn = new Button("购买");
+
+    public Frame1()
+    {
+        this.setSize(400, 200);
+        this.setLayout(new FlowLayout());
+        this.add(btn);
+        this.add(label);
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Frame2().setMoneyListener(Frame1.this);
+            }
+        });
+        this.setVisible(true);
+    }
+
+    @Override
+    public void setMoney(String money) {
+        label.setText(money);
+    }
 }
--- src/Frame2.java
+++ src/Frame2.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Frame2 {
+public class Frame2 extends Frame {
+    private TextField textField = new TextField(20);
+    private Button btn = new Button("付款");
+    public Frame2(){
+        this.setSize(400, 200);
+        this.setLayout(new FlowLayout());
+        this.add(btn);
+        this.add(textField);
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String money = textField.getText();
+                moneyListener.setMoney(money);
+            }
+        });
+        this.setVisible(true);
+    }
+
+    private MoneyListener moneyListener;
+
+    public void setMoneyListener(MoneyListener moneyListener){
+        this.moneyListener = moneyListener;
+    }
+
+    // 接口回调
+    // 当一个对象需要给外部对象提供数据时，不直接依赖外部对象
+    // 可以定义一个内部接口把数据通过接口传递出去
+    // 所有外部对象需要这个数据时，就实现这个接口，
+    // 这样的好处是传递数据的对象不直接依赖接收数据的对象
+    // （降低耦合性）
+    public static interface MoneyListener {
+        public void setMoney(String money);
+    }
 }
