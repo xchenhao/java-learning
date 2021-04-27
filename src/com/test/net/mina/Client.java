d0b61aee9d6883726dfa600ed1b453d6da89328c
xchenhao <chenhao_apple@163.com>
2021-04-27 23:11:13 +0800

    net: mina client-server

---
+++ src/com/test/net/mina/Client.java
@@ -0,0 +1,36 @@
+package com.test.net.mina;
+
+import org.apache.mina.core.filterchain.DefaultIoFilterChainBuilder;
+import org.apache.mina.core.future.ConnectFuture;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.transport.socket.nio.NioSocketConnector;
+
+import java.net.InetSocketAddress;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        // 创建 NIO 连接
+        NioSocketConnector connector = new NioSocketConnector();
+        DefaultIoFilterChainBuilder filterChain = connector.getFilterChain();
+        filterChain.addLast("myChain", new ProtocolCodecFilter(new TextLineCodecFactory()));
+        connector.setHandler(new MinaClientHandler());
+        connector.setConnectTimeoutMillis(1000*10);
+        ConnectFuture connectFuture = connector.connect(new InetSocketAddress("localhost", 9999));// 连接服务器
+        connectFuture.awaitUninterruptibly();  // 等待连接成功
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("请输入：");
+            String info = scanner.nextLine();
+            connectFuture.getSession().write(info);  // 发送消息
+        }
+
+        // 等待服务器连接关闭，结束长连接
+        // connectFuture.getSession().getCloseFuture().awaitUninterruptibly();
+        // connector.dispose();  // 释放连接
+
+
+    }
+}
---
+++ src/com/test/net/mina/MinaClientHandler.java
@@ -0,0 +1,27 @@
+package com.test.net.mina;
+
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IoSession;
+
+public class MinaClientHandler extends IoHandlerAdapter {
+
+    @Override
+    public void sessionOpened(IoSession session) throws Exception {
+        super.sessionOpened(session);
+        System.out.println("sessionOpened");
+    }
+
+    @Override
+    public void sessionClosed(IoSession session) throws Exception {
+        super.sessionClosed(session);
+        System.out.println("sessionClosed");
+    }
+
+    @Override
+    public void messageReceived(IoSession session, Object message) throws Exception {
+        super.messageReceived(session, message);
+        String msg = (String) message;
+        System.out.println(msg);
+
+    }
+}
