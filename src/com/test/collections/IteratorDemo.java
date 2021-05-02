b638242a8184c3a144d8ac4761b1aa46dea4d03f
xchenhao <chenhao_apple@163.com>
2021-05-02 19:56:13 +0800

    collections: function interface

--- src/com/test/collections/IteratorDemo.java
+++ src/com/test/collections/IteratorDemo.java
@@ -4,11 +4,67 @@ import org.junit.Test;
 
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 /**
  * 集合的迭代
  */
 public class IteratorDemo {
+
+    //
+    @Test
+    public void predicateTest()
+    {
+        List<String> list = Arrays.asList("Larry", "Moe", "Curly", "Tom", "Bob");
+        List<String> data = filter(list, (s) -> s.contains("o"));
+        data.forEach(System.out::println);
+    }
+
+    private static List<String> filter(List<String> list, Predicate<String> p)
+    {
+        List<String> result = new ArrayList<>();
+        for (String str: list) {
+            if (p.test(str)) {  // 测试是否符合要求
+                result.add(str);
+            }
+        }
+
+        return result;
+    }
+
+    // Supplier 代表结果供应商
+    @Test
+    public void supplierTest()
+    {
+        List<Integer> list = getNums(10, () -> (int)(Math.random() * 100));
+        list.forEach(System.out::println);
+    }
+
+    public static List<Integer> getNums(int num, Supplier<Integer> supplier)
+    {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            list.add(supplier.get());
+        }
+
+        return list;
+    }
+
+    // Function 表示接收一个参数并产生一个结果的函数
+    @Test
+    public void functionTest()
+    {
+        // String s = strToUpper("tom", (s) -> s.toUpperCase());
+        String s = strToUpper("tom", String::toUpperCase);
+        System.out.println(s);
+    }
+
+    public static String strToUpper(String str, Function<String, String> f)
+    {
+        return f.apply(str);
+    }
 
     /**
      * JDK 1.8 新的迭代方法
